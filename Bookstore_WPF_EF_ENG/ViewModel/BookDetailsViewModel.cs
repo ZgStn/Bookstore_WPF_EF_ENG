6b4fc216f5f9cc463fc132d7567a4f7e5cc10580
Frida Padellaro <frida.padellaro@gmail.com>
2026-01-07 16:22:38 +0100

    make LoadQuantity() async

--- Bookstore_WPF_EF_ENG/ViewModel/BookDetailsViewModel.cs
+++ Bookstore_WPF_EF_ENG/ViewModel/BookDetailsViewModel.cs
@@ -1,24 +1,27 @@
 ﻿using System.Collections.ObjectModel;
 using Bookstore.Domain;
 using Bookstore.Infrastructure.Data.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookstore_WPF_EF_ENG.ViewModel
 {
-    internal class BookDetailsViewModel
+    internal class BookDetailsViewModel : ViewModelBase
     {
         public ObservableCollection<Inventory> Details { get; set; } //TODO: vi har samma observablecollection i mainwindowviewmodel, blir det ett problem?
         public BookDetailsViewModel(Inventory inventory)
         {
-            LoadQuantity(inventory.Quantity);
+            _ = LoadQuantityAsync(inventory.Quantity);
         }
 
-        private void LoadQuantity(int quantity)
+        private async Task LoadQuantityAsync(int quantity)
         {
             using var db = new BookstoreContext();
 
             Details = new ObservableCollection<Inventory>(
-                db.Inventories.Where(i => i.Quantity == quantity).ToList()
+                await db.Inventories.Where(i => i.Quantity == quantity).ToListAsync()
                 );
+
+            RaisePropertyChanged(nameof(Details));
         }
     }
 }
